e3e134f8a8d476157cceaae06bf940135e4ad4d2
Andrey Yunin <andrey.yunin@gmail.com>
2026-05-18 10:53:25 +0300

    Complete Homework 01: Kernel configuration and build

---
+++ my_kernel_config.sh
@@ -0,0 +1,84 @@
+ #!/bin/bash
+
+# Функции-хелперы в стиле оригинального скрипта
+function disable() {
+     echo -e "ОТКЛЮЧЕНИЕ модуля $1"
+     ./scripts/config --disable "$1"
+}
+
+function enable() {
+     echo -e "\nВКЛЮЧЕНИЕ модуля $1\n"
+     ./scripts/config --enable "$1"
+}
+
+function set_val() {
+     echo -e "\nУСТАНОВКА $1 = $2\n"
+     ./scripts/config --set-val "$1" "$2"
+}
+
+function finalize_config() {
+     echo "--- Применение изменений и проверка зависимостей ---"
+     make olddefconfig
+}
+
+################ START SCRIPT ####################
+
+echo "=== Настройка конфигурации по презентации 0x02 ==="
+
+# 1. Отключение безопасности (Слайды 13-14)
+disable "SECURITY_SELINUX"
+disable "SECURITY_SMACK"
+disable "SECURITY_TOMOYO"
+disable "SECURITY_APPARMOR"
+disable "SECURITY_YAMA"
+
+# 2. Отключение KASLR (Слайд 15)
+disable "RANDOMIZE_BASE"
+
+# 3. Отключение защит процессора и оптимизация (Слайд 16)
+disable "CPU_MITIGATIONS"
+disable "MITIGATION_SPECTRE_BHI"
+disable "MITIGATION_RFDS"
+disable "PAGE_TABLE_ISOLATION"
+disable "ZSWAP"
+
+# 4. Отключение BPF (Слайд 17)
+disable "BPF"
+disable "BPF_SYSCALL"
+disable "BPF_JIT"
+disable "BPF_EVENTS"
+disable "BPFILTER"
+
+# 5. Включение инструментов отладки (Слайды 19-21)
+enable "DEBUG_FS"
+enable "FTRACE"
+enable "FUNCTION_TRACER"
+enable "DYNAMIC_FTRACE"
+enable "FUNCTION_GRAPH_TRACER"
+enable "STACK_TRACER"
+enable "KUNIT"
+enable "KUNIT_TEST"
+enable "KASAN"
+enable "STACKTRACE"
+enable "KASAN_GENERIC"
+enable "KASAN_INLINE"
+enable "KASAN_EXTRA_INFO"
+enable "KGDB"
+enable "KGDB_SERIAL_CONSOLE"
+enable "CONSOLE_POLL"
+enable "KPROBES"
+enable "KPROBE_EVENT"
+
+# 6. Настройка отладочной информации
+disable "DEBUG_INFO_NONE"
+enable "DEBUG_INFO"
+set_val "CONFIG_DEBUG_INFO_DWARF5" "y"
+
+# Финализация
+finalize_config
+
+# Проверка (как в оригинальном скрипте)
+echo -e "\nПроверка критических настроек:"
+grep "CONFIG_DEBUG_INFO" .config
+grep "CONFIG_KASAN" .config
+grep "CONFIG_RANDOMIZE_BASE" .config
